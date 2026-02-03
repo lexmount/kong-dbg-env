4e50cf64a77fcd18c27ee06dfc848deb1f3d738d
freelw <freelw81@qq.com>
2026-02-03 14:20:51 +0800

    update

---
+++ init.sql
@@ -0,0 +1,8 @@
+create database  k8s_kong_dbg;
+\c k8s_kong_dbg;
+
+CREATE USER k8s_kong_dbg_user WITH PASSWORD 'qYs21KT62BqyMGeW70U9';
+GRANT ALL PRIVILEGES ON DATABASE k8s_kong_dbg TO k8s_kong_dbg_user;
+GRANT ALL ON SCHEMA public TO k8s_kong_dbg_user;
+
+psql -h 10.2.62.142 -p 5432 -U k8s_kong_dbg_user -d k8s_kong_dbg
